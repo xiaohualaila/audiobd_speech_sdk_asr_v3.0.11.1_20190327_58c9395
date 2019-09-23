76507e7343bdb8e6f176690a78f9df9db4d9c5e5
38012 <380129462@qq.com>
2019-09-23 17:01:58 +0800

    修改更新

--- app/src/main/java/com/aier/speech/recognizer/mvp/activity/StartActivity.java
+++ app/src/main/java/com/aier/speech/recognizer/mvp/activity/StartActivity.java
@@ -1,65 +1,43 @@
 package com.aier.speech.recognizer.mvp.activity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
+
 import com.aier.speech.recognizer.R;
 import com.aier.speech.recognizer.mvp.contract.StartContract;
 import com.aier.speech.recognizer.mvp.presenter.StartPresenter;
-import com.aier.speech.recognizer.util.ToastyUtil;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-
+import java.io.File;
+import java.io.IOException;
 import butterknife.BindView;
 
-public class StartActivity extends BaseActivity implements StartContract.View {
+public class StartActivity extends BaseActivity implements StartContract.View, SurfaceHolder.Callback {
     private static String TAG = "StartActivity";
-    @BindView(R.id.tv_time)
-    TextView tv_time;
-    @BindView(R.id.tv_date)
-    TextView tv_date;
-    @BindView(R.id.tv_wendu)
-    TextView tv_wendu;
-    @BindView(R.id.tv_weather)
-    TextView tv_weather;
+    @BindView(R.id.sf_video)
+    SurfaceView surfaceView;
     private StartPresenter presenter;
-    private boolean openCVIsOk = false;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceHolder myholder;
+    private int currentPosition = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         presenter = new StartPresenter(this);
         presenter.getTime();
         presenter.getWeather();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
        // startDelay();
+        myholder = surfaceView.getHolder();
+        myholder.addCallback(this);
+        mMediaPlayer = new MediaPlayer();
     }
 
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                    Log.i("rr", "OpenCV loaded successfully");
-                    openCVIsOk = true;
-                    break;
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
-        }
-    };
 
     private void startDelay() {
         new Handler().postDelayed(() -> {
@@ -91,29 +69,80 @@ public class StartActivity extends BaseActivity implements StartContract.View {
     public void openIv6(View view) {
        // startActivity(new Intent(this, .class));
     }
-    public void returnRedTime(View view) {
-        if(openCVIsOk){
-            startActivity(new Intent(this, CameraActivity.class));
-        }else {
-            ToastyUtil.INSTANCE.showError("openCV初始化失败！，请重启软件。");
-        }
+//    public void returnRedTime(View view) {
+//        if(openCVIsOk){
+//            startActivity(new Intent(this, CameraActivity.class));
+//        }else {
+//            ToastyUtil.INSTANCE.showError("openCV初始化失败！，请重启软件。");
+//        }
+//
+//    }
 
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        presenter.dispose();
-    }
 
     @Override
     public void backTime(String time,String date) {
-        tv_time.setText(time);
-        tv_date.setText(date);
+//        tv_time.setText(time);
+//        tv_date.setText(date);
     }
 
     @Override
     public void getWeatherDataSuccess(String weather, String wendu) {
-        tv_weather.setText(weather);
-        tv_wendu.setText(wendu);
+//        tv_weather.setText(weather);
+//        tv_wendu.setText(wendu);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mMediaPlayer.setDisplay(myholder);    //设置显示视频显示在SurfaceView上
+        play(currentPosition);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
+            currentPosition = mMediaPlayer.getCurrentPosition();
+            mMediaPlayer.stop();
+        }
+    }
+
+
+
+    private void play(int msec){
+            File file = new File(Environment.getExternalStorageDirectory()
+            + "/Download/", "test.mp4");
+
+        try {
+            mMediaPlayer.reset();
+            mMediaPlayer.setDataSource(file.getPath());
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnPreparedListener(mp -> {
+                mMediaPlayer.start();
+                mMediaPlayer.seekTo(msec);
+            });
+            mMediaPlayer.setOnCompletionListener(mp -> {
+                mMediaPlayer.start();
+                mp.setLooping(true);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.dispose();
+        if(mMediaPlayer != null){
+            finish();
+            mMediaPlayer.reset();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
     }
 }
