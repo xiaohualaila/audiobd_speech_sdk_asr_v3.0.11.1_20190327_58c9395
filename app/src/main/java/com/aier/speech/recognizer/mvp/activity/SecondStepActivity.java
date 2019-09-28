9f976bdfda607d48598d53e2917971c4a462972b
38012 <380129462@qq.com>
2019-09-28 16:01:07 +0800

    修改更新

--- app/src/main/java/com/aier/speech/recognizer/mvp/activity/SecondStepActivity.java
+++ app/src/main/java/com/aier/speech/recognizer/mvp/activity/SecondStepActivity.java
@@ -1,5 +1,7 @@
 package com.aier.speech.recognizer.mvp.activity;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
@@ -7,23 +9,32 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.TextView;
+
 import com.aier.speech.recognizer.R;
 import com.aier.speech.recognizer.weight.ChooseView;
 
 import java.io.File;
 import java.io.IOException;
+
 import butterknife.BindView;
 
 
-public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Callback,ChooseView.ChooseCallBack {
+public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Callback, ChooseView.ChooseCallBack {
     private static String TAG = "SecondStepActivity";
     @BindView(R.id.sf_video)
     SurfaceView surfaceView;
     @BindView(R.id.choose_view)
     ChooseView choose_view;
+    @BindView(R.id.text_title)
+    TextView text_title;
+    @BindView(R.id.text)
+    TextView text;
+
     private MediaPlayer mMediaPlayer;
     private SurfaceHolder myholder;
     private int currentPosition = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,11 @@ public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Ca
         myholder.addCallback(this);
         mMediaPlayer = new MediaPlayer();
         choose_view.setChooseCallBack(this);
+        AssetManager assets = getAssets();
+        Typeface tf = Typeface.createFromAsset(assets, "fonts/MStiffHeiPRC.ttf");
+        text_title.setTypeface(tf);
+        text.setTypeface(tf);
+        setTextContent();
     }
 
     @Override
@@ -57,13 +73,13 @@ public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Ca
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
             currentPosition = mMediaPlayer.getCurrentPosition();
             mMediaPlayer.stop();
         }
     }
 
-    private void play(int msec){
+    private void play(int msec) {
         File file = new File(Environment.getExternalStorageDirectory()
                 + "/Download/", "test.mp4");
 
@@ -87,7 +103,7 @@ public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Ca
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mMediaPlayer != null){
+        if (mMediaPlayer != null) {
             mMediaPlayer.reset();
             mMediaPlayer.release();
             mMediaPlayer = null;
@@ -96,6 +112,20 @@ public class SecondStepActivity extends BaseActivity implements SurfaceHolder.Ca
 
     @Override
     public void setCallBack(int num) {
-        Log.i("sss","+++++++++num " +num);
+        Log.i("sss", "+++++++++num " + num);
+        if (num == 1) {
+            setTextContent();
+        } else if (num == 2) {
+            text_title.setText("视觉分拣");
+            text.setText("通过AI深度的视觉监测，对桃子的着色度，大小，破损情况，虫蛀情况及含糖量等进行检测。与下单系统联动，分选至不同对的下果平台和包装路线。");
+        } else if(num == 3) {
+            text_title.setText("智能码垛说明");
+            text.setText("自动封箱机封箱完成后，码垛机器人统一码放，由AGV叉车转运至水果排放区 ，智联下单系统和物流系统，运送至顾客之手。");
+        }
+    }
+
+    public void setTextContent() {
+        text_title.setText("无人搬运");
+        text.setText("AVG小车将成像的桃子从储桃区运送到物料传送带上，再由机器自动翻箱倾倒在自动上果机上，自动上果机将桃子输送到等级分选机中，调整桃子姿态，到达检测区");
     }
 }
