fc702966b7d4264b1c785e7124a9f15554063dfc
38012 <380129462@qq.com>
2019-10-09 16:49:45 +0800

    修改更新

--- app/src/main/java/com/aier/speech/recognizer/broadcastReceiver/BootBroadcastReceiver.java
+++ app/src/main/java/com/aier/speech/recognizer/broadcastReceiver/BootBroadcastReceiver.java
@@ -3,7 +3,7 @@ package com.aier.speech.recognizer.broadcastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import com.aier.speech.recognizer.mvp.activity.StartActivity;
+import com.aier.speech.recognizer.mvp.activity.FirstActivity;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
     public static final String ACTION = "android.intent.action.BOOT_COMPLETED";
@@ -11,7 +11,7 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ACTION)) {
-            Intent in = new Intent(context, StartActivity.class);
+            Intent in = new Intent(context, FirstActivity.class);
             in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(in);
         }
